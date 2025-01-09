b93124de2988256a9aa79279efd16461645ddcac
Armaanilyas <armaanilyas2005@gmail.com>
2025-01-09 14:56:54 +0000

    created 4 new pages, need to link them to burger menu... Also added a bulma button that takes the user to one of the 4 new pages I have made, depending on the season, to show them some perfume recommendations

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+
+    path('Spring', views.Spring, name='Spring Perfume Recommendations'),
+
+    path('Summer', views.Summer, name='Summer Recommendations'),
+    path('Winter', views.Winter, name='Winter Recommendations'),
+    path('Fall', views.Autumn, name='Autumn Recommendations'),
     ]
