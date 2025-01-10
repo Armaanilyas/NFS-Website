cac1a93570988b8c636cac1a2705a821383dd7e0
Armaanilyas <armaanilyas2005@gmail.com>
2025-01-10 10:26:48 +0000

    retrying adding featured perfume dropdown functionality

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,10 +5,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-
-    path('Spring', views.Spring, name='Spring Perfume Recommendations'),
-
-    path('Summer', views.Summer, name='Summer Recommendations'),
-    path('Winter', views.Winter, name='Winter Recommendations'),
-    path('Fall', views.Autumn, name='Autumn Recommendations'),
     ]
